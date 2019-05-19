f31b1e5c412d22903491256aa9a38be16df37216
Ostrzyciel <magwac69@gmail.com>
2019-05-19 12:12:14 +0200

    Do ThiccModelAggregateContent logic
    bug: T223695
    Change-Id: Icc70def401c3ed8ff202e589690696b5eb2d5cdb

--- includes/ThiccModelAggregateContent.php
+++ includes/ThiccModelAggregateContent.php
@@ -5,6 +5,24 @@
  */
 class ThiccModelAggregateContent extends JsonContent {
 
+	/** @var bool Whether contents have been populated */
+	protected $decoded = false;
+
+	/** @var array */
+	protected $metadata;
+
+	/** @var string */
+	protected $introduction;
+
+	/** @var array Threads associated to this page */
+	protected $thiccness;
+
+	/** @var string How to display contents */
+	protected $displaymode;
+
+	/** @var string Error message text */
+	protected $errortext;
+
 	/**
 	 * @param string $text
 	 */
@@ -12,4 +30,105 @@ class ThiccModelAggregateContent extends JsonContent {
 		parent::__construct( $text, 'ThiccModelAggregateContent' );
 	}
 
+	/**
+	 * Decode and validate the contents
+	 * @return bool Whether the contents are valid
+	 */
+	public function isValid() {
+		$jsonParse = $this->getData();
+		if ( $jsonParse->isGood() ) {
+			// TODO: Check schema, etc
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Decode the JSON contents and populate protected variables
+	 */
+	protected function decode() {
+		if ( $this->decoded ) {
+			return;
+		}
+
+		$jsonParse = $this->getData();
+		$data = $jsonParse->isGood() ? $jsonParse->getValue() : null;
+		if ( $data ) {
+			if ( !$this->isValid() ) {
+				$this->displaymode = 'error';
+				if ( !parent::isValid() ) {
+					// It's not even valid json
+					$this->errortext = htmlspecialchars(
+						$this->getText()
+					);
+				} else {
+					$this->errortext = FormatJson::encode(
+						$data,
+						true,
+						FormatJson::ALL_OK
+					);
+				}
+			} else {
+				$this->metadata = $data->metadata ?? [];
+				$this->introduction = $data->introduction ?? [];
+				$this->thiccness = $data->thiccness ?? [];
+			}
+		}
+
+		$this->decoded = true;
+	}
+
+	/**
+	 * Fill $output with information derived from the content.
+	 *
+	 * @param Title $title
+	 * @param int $revId
+	 * @param ParserOptions $options
+	 * @param bool $generateHtml
+	 * @param ParserOutput &$output
+	 */
+	protected function fillParserOutput( Title $title, $revId,
+		ParserOptions $options, $generateHtml, ParserOutput &$output
+	) {
+		$this->decode();
+		$html = '';
+
+		// If error, then bypass all this and just show the offending JSON
+		if ( $this->displaymode == 'error' ) {
+			$html = '<div class=errorbox>'
+				. wfMessage( 'invalid' )
+				. "</div>\n<pre>"
+				. $this->errortext
+				. '</pre>';
+		} else {
+			$html .= $this->introduction;
+			$html .= Html::element('br');
+
+			foreach ( $this->thiccness as $thicc ) {
+
+				$title = Title::newFromText( $thicc->thread, NS_THICC );
+				$page = WikiPage::factory( $title );
+				$content = $page->getContent();
+
+				$html .= $content->getContent();
+			}
+
+			// Thread styles, check later if we even have any threads to load this
+			$output->addModuleStyles( [ 'ext.thicc.threads' ] );
+
+
+//			$html .= Html::openElement(
+//				'div',
+//				[ 'class' => 'thicc-thread' ]
+//			);
+//			$html .= Html::element( 'h2', [], $this->displayName );
+//
+//			// do first comment
+//			$html .= $this->renderComment( $this->comment );
+//
+//			$html .= Html::closeElement( 'div' );
+		}
+
+		$output->setText( $html );
+	}
 }
--- includes/ThiccModelAggregateContentHandler.php
+++ includes/ThiccModelAggregateContentHandler.php
@@ -18,4 +18,55 @@ class ThiccModelAggregateContentHandler extends JsonContentHandler {
 
 	}
 
+	/**
+	 * @return ThiccModelAggregateContent
+	 */
+	public function makeEmptyContent() {
+		$empty = <<<JSON
+			{
+				"metadata": [],
+				"introduction": "",
+				"thiccness": [
+				]
+			}
+JSON;
+		return new ThiccModelAggregateContent( $empty );
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function getContentClass() {
+		return 'ThiccModelAggregateContent';
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isParserCacheSupported() {
+		return true;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function supportsRedirects() {
+		return true;
+	}
+
+	/**
+	 * Turns ThiccModelAggregateContent page into redirect
+	 *
+	 * Note that wikitext redirects are created, as generally, this content model
+	 * is used in namespaces that support wikitext, and wikitext redirects are
+	 * expected.
+	 *
+	 * @param Title $destination The page to redirect to
+	 * @param string $text Text to include in the redirect.
+	 * @return Content
+	 */
+	public function makeRedirectContent( Title $destination, $text = '' ) {
+		$handler = new WikitextContentHandler();
+		return $handler->makeRedirectContent( $destination, $text );
+	}
 }
